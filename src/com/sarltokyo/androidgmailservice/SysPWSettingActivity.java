7ec8c7e9e4ad894b89a7d7ba0054adb046e1a722
andropenguin <andropenguin@gmail.com>
2011-12-01 10:20:56 +0900

    add license statements

--- src/com/sarltokyo/androidgmailservice/SysPWSettingActivity.java
+++ src/com/sarltokyo/androidgmailservice/SysPWSettingActivity.java
@@ -1,5 +1,30 @@
 package com.sarltokyo.androidgmailservice;
 
+/**
+ * SysPWSettingActivity.java
+ *
+ * Copyright (C) 2011
+ * @author OSABE Satoshi, e-mail address: andropenguin@gmail.com
+ * @version 1.0
+ *
+ *  This Program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version with the exception of the file
+ *  FlvDownloadServiceInterface.aidl. The license of the
+ *  FlvDownloadServiceInterface.aidl is Public domain. About the
+ *  exclamation of this license, see http://en.wikipedia.org/wiki/Public_domain.
+ *  This Program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with FlvDownlaodService.java; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  http://www.gnu.org/copyleft/gpl.html
+ */
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
